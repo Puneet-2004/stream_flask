192ea85640ee34972da9c6e2685d36b71e0a0bb9
Puneet-2004 <puneetjavaji@gmail.com>
2025-04-04 10:08:24 +0530

    CHANGED app.py

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
